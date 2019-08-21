0f8ac0b61d58141d2487c0edf5240f9f11673126
Bryan0xFF <bjmejia@correo.url.edu.gt>
2019-08-21 11:29:03 -0600

    first implementation of Lexical Rules added to DFA

---
+++ LexicalAnalyzer/src/lexCode/MainClass.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lexCode;
+
+import java.io.File;
+import jflex.*;
+
+/**
+ *
+ * @author Bryan
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        String path = "C:/Users/Bryan/Documents/NetBeansProjects/LexicalAnalyzer/src/lexCode/Lexer.flex";
+        GenLex(path);
+    }
+    
+    public static void GenLex(String ruta){
+        File archivo = new File(ruta);
+        jflex.Main.generate(archivo);
+    }
+}
---
+++ LexicalAnalyzer/src/lexCode/Tokens.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lexCode;
+
+/**
+ *
+ * @author Bryan
+ */
+public enum Tokens {
+    
+    OPERADORES,
+    RESERVADAS,
+    IDENTIFICADORES,
+    INT,
+    FLOAT,
+    BIT,
+    STRING,
+    ERROR
+    
+}
